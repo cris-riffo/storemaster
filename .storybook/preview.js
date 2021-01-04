ac538f47ca5c66137f840137927edd6e4af64702
Cristian Riffo <riffo.cris@gmail.com>
2021-01-05 12:30:45 +1300

    Removing comments

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,3 @@
-// .storybook/preview.js
-
-// import React from "react";
-// import { ThemeProvider } from 'styled-components';
-
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
-
-// export const decorators = [
-//   (Story) => (
-//     <ThemeProvider theme="default">
-//       <Story />
-//     </ThemeProvider>
-//   ),
-// ];
